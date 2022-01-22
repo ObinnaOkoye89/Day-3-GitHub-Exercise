114ad2b94b2fe78f7091a148dc8cb875bf305986
Obinna Okoye <okokoye@bradford.ac.uk>
2022-01-22 18:17:16 +0000

    created ratio calc

--- calculate_ratio.py
+++ calculate_ratio.py
@@ -1,4 +1,9 @@
-import ________
+import Calculator
 
 # Allow the user to define two numbers a & b
+a = int(input("First number for the ratio opertaion?"))
+b = int(input("Second number to the ratio operation?"))
+
 # Calculate the ratio by importing your function ratio
+result = Calculator.Ratio(a,b)
+print(result)
