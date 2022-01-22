28c1302f0e37a73ec0fdbd58b2913a23d459f478
Obinna Okoye <okokoye@bradford.ac.uk>
2022-01-22 16:47:34 +0000

    changes made

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,33 @@
 a = int(input("What would you like your first number to be?"))
 b = int(input("What would you like your second number to be?"))
 
-c = a + b
+
+def Addition(a, b):
+    return a + b
+    
+    
+def Subtraction(a, b):
+    return a - b
+    
+
+def Multiplication(a, b):
+    return a * b
+    
+   
+def Division(a, b):
+    return a / b
+    
+def Ratio(a,b): 
+    return (a/c)*100
+
+c = Addition(a,b)
+d = Subtraction(a,b)
+e = Multiplication(a,b)
+f = Division(a,b)
+g = Ratio(a,b)
+
+print(f" {c} is the sum of {a} and {b}")
+print(f"{d} is the subtraction of {b} from {a}")
+print(f"{e} is the multiplication of {a} and {b}")
+print(f" {f} is the division of {a} by {b}")
+print(f"The percentage of {a} to {b} is {int(g)}")
